11be9afd3acbc2455ba57addfc6c5ffd3dd15103
Documenter.jl <documenter@juliadocs.github.io>
2023-05-08 13:01:54 +0000

    build based on 643f133

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DGGS","category":"page"},{"location":"#DGGS","page":"Home","title":"DGGS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DGGS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DGGS]","category":"page"}]
+}
