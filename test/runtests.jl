e9c0ec10a299e4a09af14a018c7eb077b262eff2
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-05-09 10:52:06 +0200

    Add test for DGGRID7_jll

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using DGGS
 using Test
 
 @testset "DGGS.jl" begin
-    # Write your tests here.
+    using DGGRID7_jll
+    dggrid() do dggrid_path
+        run(`env`)
+        run(`$dggrid_path`)
+    end
 end
+
+const dggrid_binpath = @generate_wrappers(DGGRID7_jll)
