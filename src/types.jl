ecd3104a671d8b9988b5d5d6c731ca488a04e0ca
Daniel Loos <dloos@bgc-jena.mpg.de>
2024-04-25 11:56:17 +0200

    Remove type GeoCube

--- src/types.jl
+++ src/types.jl
@@ -15,19 +15,6 @@ struct CellCube
     level::Integer
 end
 
-struct GeoCube
-    data::YAXArray
-
-    function GeoCube(data)
-        :lon in propertynames(data) || error("Axis with name :lon must be present")
-        :lat in propertynames(data) || error("Axis with name :lat must be present")
-        -180 <= minimum(data.lon) <= maximum(data.lon) <= 180 || error("All longitudes must be within [-180, 180]")
-        -90 <= minimum(data.lat) <= maximum(data.lat) <= 90 || error("All latitudes must be within [-90, 90]")
-
-        new(data)
-    end
-end
-
 struct GridSystem
     data::Dict{Int,CellCube} # some levels might be skipped
 
