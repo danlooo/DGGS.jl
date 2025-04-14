4965c1fddf9282adae08a25fede2ba2c77c4dc26
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-04-14 14:35:16 +0200

    Add DGGS.jl

---
+++ src/DGGS.jl
@@ -0,0 +1,20 @@
+module DGGS
+
+import Proj
+using Base.Threads
+using YAXArrays
+using DimensionalData
+using Statistics
+
+include("types.jl")
+include("cells.jl")
+include("arrays.jl")
+
+function __init__()
+    @info "DGGS.jl initialized with $(nthreads()) threads"
+    global transformations = [Proj.Transformation(crs_geo, crs_isea; ctx=Proj.proj_context_create()) for _ in 1:nthreads()]
+    global inv_transformations = [Proj.Transformation(crs_isea, crs_geo; ctx=Proj.proj_context_create()) for _ in 1:nthreads()]
+end
+
+export to_cell, to_geo, to_dggs_array, Cell
+end
