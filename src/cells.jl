e897d3efa45cf47d71c6eb71be28fb614e988fdb
Fabian Gans <fabiancoding@gmail.com>
2025-05-05 16:17:58 +0200

    Some improvements (#139)
    - Reduce memory allocations using StaticArrays
    - Use JuliaGeometry/CoordinateTransformations.jl for affine transformation-
    - Ensure thread safety using Channel
    
    
    Commits:
    
    * Some performance updates
    
    * up manifest
    
    * First changes
    
    * Reimplement init
    
    * typo
    
    * Fix Cell from int reconstruction
    
    * fix unit tests
    
    * add github CI
    
    * revert to old test case
    
    * allow nd-arrays also in to_geo

--- src/cells.jl
+++ src/cells.jl
@@ -1,28 +1,99 @@
 using DimensionalData.Dimensions: dimcolors
-
+using StaticArrays
 #
 # grid topology
 #
 
-n_submats = 5
-crs_geo = "EPSG:4326"
-crs_isea = "+proj=isea +orient=isea +mode=plane +R=6371007.18091875"
+const n_submats = 5
+const crs_geo = "EPSG:4326"
+const crs_isea = "+proj=isea +orient=isea +mode=plane +R=6371007.18091875"
 
 # see https://www.youtube.com/watch?v=FWJOdMh8JQo&t=341s
-θ_rotation = 60 * π / 180
-rotation_matrix = [cos(θ_rotation) -sin(θ_rotation); sin(θ_rotation) cos(θ_rotation)]
-θ_skew = 30 * π / 180
-skew_matrix = [1 tan(θ_skew); 0 1]
-trans_matrix = skew_matrix * rotation_matrix
+function _compute_trans_matrix()
+    θ_rotation = 60 * π / 180
+    rotation_matrix = @SMatrix [cos(θ_rotation) -sin(θ_rotation); sin(θ_rotation) cos(θ_rotation)]
+    θ_skew = 30 * π / 180
+    skew_matrix = @SMatrix [1 tan(θ_skew); 0 1]
+    skew_matrix * rotation_matrix
+end
+const trans_matrix = _compute_trans_matrix()
+const itrans_matrix = inv(trans_matrix)
 
-trans = Proj.Transformation(crs_geo, crs_isea)
-inv_trans = inv(trans)
-epsilon = 1e-3 # move point from vertix into quad
+LatLonToISEA() = Proj.Transformation(crs_geo, crs_isea)
+
+ISEAToLatLon() = inv(LatLonToISEA())
+const epsilon = 1e-3 # move point from vertix into quad
+
+struct ISEAToRotatedISEA <: CoordinateTransformations.Transformation end
+struct RotatedISEAToISEA <: CoordinateTransformations.Transformation end
+
+function (::ISEAToRotatedISEA)(x_isea, y_isea)
+    # rectify
+    x_rect, y_rect = trans_matrix * @SMatrix [x_isea; y_isea]
+
+    # affine transformation
+    x_scaled = (x_rect - x_rect_min) / rect_width * (n_submats + 1)
+    y_scaled = (y_rect - y_rect_min) / rect_height * n_submats
+
+    n_cell = (y_rect - y_rect_min) / rect_height * n_submats |> floor |> Int
+    x_offset = x_scaled - n_cell
+    y_offset = y_scaled - n_cell
+
+
+    return clamp(n_cell, 0, 4), x_offset, y_offset
+end
+(f::ISEAToRotatedISEA)(t::Tuple) = f(t...)
+
+function (::RotatedISEAToISEA)(n_cell, x_scaled, y_scaled)
+    # Reverse transformation
+    x_rect = (x_scaled + n_cell) / 6 * rect_width + x_rect_min
+    y_rect = (y_scaled + n_cell) / 5 * rect_height + y_rect_min
+
+    x_isea, y_isea = itrans_matrix * @SMatrix [x_rect; y_rect]
+    x_isea, y_isea
+end
+(f::RotatedISEAToISEA)(t::Tuple) = f(t...)
+
+struct RotatedISEAToIndices <: CoordinateTransformations.Transformation
+    resolution::Int
+end
+function (r::RotatedISEAToIndices)(x_offset, y_offset)
+    # Discretize coordinates to cell ids
+    i_cell = x_offset * 2^r.resolution |> floor |> Int
+    j_cell = y_offset * 2^r.resolution |> floor |> Int
+
+    # crop
+    # TODO: review
+    i_cell = clamp(i_cell, 0, 2 * 2^r.resolution - 1)
+    j_cell = clamp(j_cell, 0, 2^r.resolution - 1)
+    i_cell, j_cell
+end
+(r::RotatedISEAToIndices)(t::Tuple) = r(t...)
+
+
+
 # use icosahedron vertices to calculate the extent
-x_rect_min, y_rect_min = trans_matrix * vcat(trans(58.2825256, -168.75)...)
-x_rect_max, y_rect_max = trans_matrix * vcat(trans(0.0 + epsilon, -137.0325256 - epsilon)...)
-rect_width = x_rect_max - x_rect_min
-rect_height = y_rect_max - y_rect_min
+function compute_rectangles()
+    corner1 = LatLonToISEA()(58.2825256, -168.75)
+    corner2 = LatLonToISEA()(0.0 + epsilon, -137.0325256 - epsilon)
+    x_rect_min, y_rect_min = trans_matrix * @SVector [corner1[1], corner1[2]]
+    x_rect_max, y_rect_max = trans_matrix * @SVector [corner2[1], corner2[2]]
+    rect_width = x_rect_max - x_rect_min
+    rect_height = y_rect_max - y_rect_min
+    x_rect_min, rect_width, y_rect_min, rect_height
+end
+
+const x_rect_min, rect_width, y_rect_min, rect_height = compute_rectangles()
+const rect_borders = @SVector [
+    (-2.1104759358105145e7, -5.75584346120373e6, -1.6283372582998954e7, -8.307844429392608e6)
+    (-1.3430301409731183e7, 1.9186144871702371e6, -9.637109526020896e6, -1.6615688858253537e6)
+    (-5.755843461357219e6, 9.5930724355442e6, -2.9908464690428358e6, 4.984706657741904e6)
+    (1.9186144870167463e6, 1.726753038391816e7, 3.6554165879352223e6, 1.1630982201309163e7)
+    (9.593072435390707e6, 2.4941988332292132e7, 1.0301679644913286e7, 1.8277257744876422e7)
+]
+
+#const x_rect_min, x_rect_max, rect_width, y_rect_min, y_rect_max, rect_height = compute_rectangles()
+
 
 #
 # coordinate transformations
@@ -42,29 +113,13 @@ function to_cell(lon::Real, lat::Real, resolution)
     lat == 90 && return Cell(0, 0.5 * 2^resolution, 0, resolution)
 
     # project to ISEA
-    trans = transformations[threadid()]
+    trans = take!(transformations)
     x_isea, y_isea = trans(lat, lon)
+    put!(transformations, trans)
 
-    # rectify
-    x_rect, y_rect = trans_matrix * [x_isea; y_isea]
-    n_cell = (y_rect - y_rect_min) / rect_height * n_submats |> floor |> Int
+    n_cell, x_offset, y_offset = ISEAToRotatedISEA()(x_isea, y_isea)
 
-    # affine transformation
-    x_scaled = (x_rect - x_rect_min) / rect_width * (n_submats + 1)
-    y_scaled = (y_rect - y_rect_min) / rect_height * n_submats
-
-    x_offset = x_scaled - (n_cell + 1) + 1
-    y_offset = y_scaled - n_cell
-
-    # Discretize coordinates to cell ids
-    i_cell = x_offset * 2^resolution |> floor |> Int
-    j_cell = y_offset * 2^resolution |> floor |> Int
-
-    # crop
-    # TODO: review
-    i_cell = clamp(i_cell, 0, 2 * 2^resolution - 1)
-    j_cell = clamp(j_cell, 0, 2^resolution - 1)
-    n_cell = clamp(n_cell, 0, 4)
+    i_cell, j_cell = RotatedISEAToIndices(resolution)(x_offset, y_offset)
 
     return Cell(i_cell, j_cell, n_cell, resolution)
 end
@@ -73,11 +128,11 @@ end
 Multi-threaded version of to_cell
 geo_points: Vector of (lon,lat) tuples
 """
-function to_cell(geo_points::Vector{Tuple{A,B}}, resolution) where {A<:Real,B<:Real}
+function to_cell(geo_points::AbstractArray{Tuple{A,B}}, resolution) where {A<:Real,B<:Real}
     # avoid overhead for small data
     length(geo_points) < 1e5 && return map(x -> to_cell(x..., resolution), geo_points)
 
-    res = Vector{Cell}(undef, length(geo_points))
+    res = similar(geo_points, Cell)
     Threads.@threads for i in eachindex(geo_points)
         lon, lat = geo_points[i][1], geo_points[i][2]
         cell = to_cell(lon, lat, resolution)
@@ -98,17 +153,16 @@ function to_geo(cell::Cell)
     cell.i == 2 * 2^cell.resolution - 1 && return (0.0, -90.0)
 
     # Reverse Discretization
-    x_scaled = (cell.i / 2^cell.resolution + cell.n)
-    y_scaled = (cell.j / 2^cell.resolution + cell.n)
+    x_scaled = cell.i / 2^cell.resolution
+    y_scaled = cell.j / 2^cell.resolution
 
     # Reverse transformation
-    x_rect = x_scaled / 6 * rect_width + x_rect_min
-    y_rect = y_scaled / 5 * rect_height + y_rect_min
-
-    x_isea, y_isea = inv(trans_matrix) * [x_rect; y_rect]
+    x_isea, y_isea = RotatedISEAToISEA()(cell.n, x_scaled, y_scaled)
 
     # Reverse ISEA projection
-    lat, lon = inv_transformations[threadid()](x_isea, y_isea)
+    inv_trans = take!(inv_transformations)
+    lat, lon = inv_trans(x_isea, y_isea)
+    put!(inv_transformations, inv_trans)
 
     # Solve pole ambiguity
     # south pole already stable
@@ -119,12 +173,11 @@ function to_geo(cell::Cell)
     end
 end
 
-function to_geo(cells::Vector{Cell{T}}) where {T<:Integer}
+function to_geo(cells::AbstractArray{Cell{T}}) where {T<:Integer}
     # avoid overhead for small data
     length(cells) < 1e5 && return map(to_geo, cells)
 
-
-    res = Vector{Tuple{Float64,Float64}}(undef, length(cells))
+    res = similar(cells, Tuple{Float64,Float64})
     Threads.@threads for i in eachindex(cells)
         geo_point = to_geo(cells[i])
         res[i] = geo_point
@@ -192,12 +245,11 @@ end
 function Cell(cell_int::Integer, resolution::Int)
     # needs resolution not stored in the integer index to ensure sequential id
     # resolution = Int((length(cell_bits) - 2) / 2) # does not work for i = 0 or j=0
-    to_int(bits) = sum([bits[x] * 2^(x - 1) for x in length(bits):-1:1])
-
-    bits = digits(cell_int, base=2, pad=2 * resolution + 4)
-    n = bits[end-2:end] |> to_int
-    j = bits[end-resolution-2:end-3] |> to_int
-    i = bits[1:end-resolution-3] |> to_int
+    n = cell_int >> (2 * resolution + 1)
+    imask = 2^(resolution + 1) - 1
+    jmask = 2^resolution - 1
+    i = cell_int & imask
+    j = (cell_int >> (resolution + 1)) & jmask
     cell = Cell(i, j, n, resolution)
     return cell
 end
