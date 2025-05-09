3513a5645428d202459795b661fc3517f2525443
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-05-09 11:16:49 +0200

    Adjust pixels to viewport

--- ext/DGGSMakie.jl
+++ ext/DGGSMakie.jl
@@ -7,20 +7,28 @@ import DimensionalData as DD
 using Makie.GeometryBasics
 using Dates
 
+"""
+Plot a DGGSArray using Makie.
+
+`resolution_scale` is a value between 0 (calculate no pixel) and 1 (calculate all pixels). Used to reduce plot time.
+"""
 function Makie.plot(
-    dggs_array::DGGSArray, args...;
+    dggs_array::DGGSArray,
+    args...;
     lon_range=-180:0.25:180,
     lat_range=-90:0.25:90,
-    resolution=1000,
+    resolution_scale::Real=1,
     kwargs...
 )
     length(DGGS.non_spatial_dims(dggs_array)) == 0 || error("DGGSArray must not have any non-spatial dimension")
+    0 <= resolution_scale <= 1 || error("resolution_scale must be between 0 and 1")
 
     fig = Figure()
     axis = Axis(fig[1, 1], limits=(-180, 180, -90, 90))
 
     data = Observable(to_geo_array(dggs_array, lon_range, lat_range))
     last_update_limits = Observable(axis.finallimits[])
+    last_update_viewport_widths = Observable(fig.scene.viewport[].widths)
 
     filtered_data = filter(x -> !ismissing(x) && !isnan(x), data[])
     cb_limits = (minimum(filtered_data), maximum(filtered_data))
@@ -30,7 +38,7 @@ function Makie.plot(
     # instead, plot one frame after the other if limits changed
     @async while true
         # skip update if limits did not change
-        if axis.finallimits[] == last_update_limits[]
+        if axis.finallimits[] == last_update_limits[] && fig.scene.viewport[].widths == last_update_viewport_widths[]
             # FPS limit to waste less computation
             sleep(Millisecond(30))
             continue
@@ -41,11 +49,12 @@ function Makie.plot(
         lon_min, lat_min = lims.origin
         lon_max, lat_max = lims.origin .+ lims.widths
 
-        lat_resolution = Int(round(lims.widths[2] / lims.widths[1] * resolution))
-        lon_range = range(lon_min, lon_max, length=resolution)
-        lat_range = range(lat_min, lat_max, length=lat_resolution)
+        lon_length, lat_length = fig.scene.viewport[].widths
+        lon_range = range(lon_min, lon_max, length=lon_length * resolution_scale)
+        lat_range = range(lat_min, lat_max, length=lat_length * resolution_scale)
         data[] = to_geo_array(dggs_array, lon_range, lat_range)
         last_update_limits[] = lims
+        last_update_viewport_widths[] = lon_length, lat_length
     end
 
     on(axis.finallimits) do lims
