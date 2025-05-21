be20a05680e09244db51aba0f6986afa6e40655f
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-05-21 10:11:11 +0200

    Allow any geo crs, including partial regions (#145)
    * Remove unused extent vals
    
    * Add compute cell array with crs
    
    * Allow to_geo_array for partial regions
    
    * Add type DGGSDataset
    
    * Harmonize Base.show for DGGS Array and Dataset
    
    * Add DGGSDataset tests
    
    * Add datasets.jl
    
    * Refactor to_dggs_dataset
    
    * Update  to_geo_dataset
    
    * Add geo bbox
    
    * Fix compute_cell_array other crs
    
    * Refactor show_after
    
    * Fix base show after
    
    * Add plot start zoom on extent
    
    * Add plot DGGSDataset RGB
    
    * Convert arrays in parallel
    
    * Update colorbar test

--- ext/DGGSMakie.jl
+++ ext/DGGSMakie.jl
@@ -7,6 +7,9 @@ import DimensionalData as DD
 using Makie.GeometryBasics
 using Dates
 
+
+using Infiltrator
+
 """
 Plot a DGGSArray using Makie.
 
@@ -15,19 +18,23 @@ Plot a DGGSArray using Makie.
 function Makie.plot(
     dggs_array::DGGSArray,
     args...;
-    lon_range=-180:0.25:180,
-    lat_range=-90:0.25:90,
+    extent=DGGS.get_geo_bbox(dggs_array),
     resolution_scale::Real=1,
     kwargs...
 )
     length(DGGS.non_spatial_dims(dggs_array)) == 0 || error("DGGSArray must not have any non-spatial dimension")
     0 <= resolution_scale <= 1 || error("resolution_scale must be between 0 and 1")
 
+    # start zoom on full extent
+    lon_range = range(extent.X..., 100)
+    lat_range = range(extent.Y..., 100)
+
     fig = Figure()
-    ax = Axis(fig[1, 1], limits=(-180, 180, -90, 90), xlabel="longitude [°]", ylabel="latitude [°]")
+    ax = Axis(fig[1, 1], limits=(extent.X..., extent.Y...), xlabel="longitude [°]", ylabel="latitude [°]")
     ax.aspect = DataAspect()
 
     data = Observable(to_geo_array(dggs_array, lon_range, lat_range))
+
     last_update_limits = Observable(ax.finallimits[])
     last_update_viewport_widths = Observable(fig.scene.viewport[].widths)
 
@@ -71,4 +78,86 @@ function Makie.plot(
     heatmap!(ax, data, colorrange=cb_limits)
     fig
 end
+
+""
+function Makie.plot(
+    ds::DGGSDataset,
+    red_layer::Symbol,
+    green_layer::Symbol,
+    blue_layer::Symbol,
+    args...;
+    extent=DGGS.get_geo_bbox(ds),
+    resolution_scale::Real=1,
+    kwargs...
+)
+    red_layer in keys(ds) || error("Red layer $(red_layer) not found in dataset")
+    green_layer in keys(ds) || error("Green layer $(green_layer) not found in dataset")
+    blue_layer in keys(ds) || error("Blue layer $(blue_layer) not found in dataset")
+
+    # start with zoom to full extent
+    lon_dim = Observable(X(range(extent.X..., 500)))
+    lat_dim = Observable(Y(range(extent.Y..., 500)))
+
+    data = @lift begin
+        ds_rgb = DGGSDataset(getproperty(ds, red_layer), getproperty(ds, green_layer), getproperty(ds, blue_layer))
+        geo_ds = to_geo_dataset(ds_rgb, $lon_dim, $lat_dim)
+
+        picture = map(CartesianIndices(($lon_dim, $lat_dim))) do i
+            r = getproperty(geo_ds, red_layer)[i]
+            g = getproperty(geo_ds, green_layer)[i]
+            b = getproperty(geo_ds, blue_layer)[i]
+
+            r = ismissing(r) ? 0 : Float64(r)
+            g = ismissing(g) ? 0 : Float64(g)
+            b = ismissing(b) ? 0 : Float64(b)
+
+            Makie.RGBf(r, g, b)
+        end
+        picture
+    end
+
+    fig = Figure()
+    ax = Axis(fig[1, 1], limits=(extent.X..., extent.Y...), xlabel="longitude [°]", ylabel="latitude [°]")
+    ax.aspect = DataAspect()
+
+    last_update_limits = Observable(ax.finallimits[])
+    last_update_viewport_widths = Observable(fig.scene.viewport[].widths)
+
+    # can't plot on every limit change
+    # instead, plot one frame after the other if limits changed
+    @async while true
+        # skip update if limits did not change
+        if ax.finallimits[] == last_update_limits[] && fig.scene.viewport[].widths == last_update_viewport_widths[]
+            # FPS limit to waste less computation
+            sleep(Millisecond(30))
+            continue
+        end
+
+        # delay plotting if small zoom/pan detected
+        change_frac = maximum(abs.(ax.finallimits[].origin .- last_update_limits[].origin) ./ last_update_limits[].widths)
+        change_frac < 0.2 && sleep(Millisecond(500))
+
+        # get limits observable once. It might change during the update
+        lims = ax.finallimits[]
+        lon_min, lat_min = lims.origin
+        lon_max, lat_max = lims.origin .+ lims.widths
+
+        lon_min = clamp(lon_min, -180, 180)
+        lon_max = clamp(lon_max, -180, 180)
+        lat_min = clamp(lat_min, -90, 90)
+        lat_max = clamp(lat_max, -90, 90)
+
+        lon_length, lat_length = fig.scene.viewport[].widths
+        lon_range = range(lon_min, lon_max, length=lon_length * resolution_scale)
+        lat_range = range(lat_min, lat_max, length=lat_length * resolution_scale)
+        data[] = to_geo_array(dggs_array, lon_range, lat_range)
+
+        last_update_limits[] = lims
+        last_update_viewport_widths[] = lon_length, lat_length
+    end
+
+    image!(ax, data)
+    fig
+end
+
 end
