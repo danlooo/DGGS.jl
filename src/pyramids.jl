154690f966702b995c52cd9c8183d0a34afd4f08
Daniel Loos <dloos@bgc-jena.mpg.de>
2026-07-15 09:56:04 +0200

    Remove redundant method

--- src/pyramids.jl
+++ src/pyramids.jl
@@ -255,15 +255,6 @@ function to_dggs_pyramid(dggs_array::DGGSArray; kwargs...)
 end
 
 function to_dggs_pyramid(
-    geo_ds::YAXArrays.Dataset, resolution::Integer, crs::String;
-    agg_func::Function, kwargs...
-)
-    dggs_ds = to_dggs_dataset(geo_ds, resolution, crs; agg_func=agg_func, kwargs...)
-    dggs_pyramid = to_dggs_pyramid(dggs_ds; agg_func=agg_func)
-    return dggs_pyramid
-end
-
-function to_dggs_pyramid(
     geo_ds::YAXArrays.Dataset,
     resolution::Integer,
     crs::String;
